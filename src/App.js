f8397dc5fcf85f5244e9bc8661a559f3ae5bc23a
dharshan0011 <dharshan.k@jumkey.com>
2022-02-03 09:43:37 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Congrats from './Congrats'
+import GuessedWords from './GuessedWords'
+import Input from './Input'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  //TODO: get props from shrared state
+  const success = false
+  const secretWord = 'party'
+  const guessedWords = []
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div data-test='component-app' className='container'>
+      <h1>Jotto</h1>
+      <Congrats success={success} />
+      <Input success={success} secretWord={secretWord} />
+      <GuessedWords guessedWords={guessedWords} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { mount } from 'enzyme'
+import App from './App'
+import { findByTestAttr } from '../test/testUtils'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// activate global mock to make getSecretWord doesn't make network call
+jest.mock('./actions')
+import { getSecretWord as mockGetSecretWord } from './actions'
+
+/**
+ * Setup function for App component
+ * @param {object} props Props for controlling the component
+ * @returns {ShallowWrapper}
+ */
+const setup = (props) => mount(<App />) //using mount because shallow doesn't trigger useEffect
+
+test('renders without error', () => {
+  const wrapper = setup()
+  const app = findByTestAttr(wrapper, 'component-app')
+  expect(app.length).toBe(1)
+})
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    //clear the mock calls from previous tests
+    mockGetSecretWord.mockClear()
+  })
+  test('getSecretWord on app mount', () => {
+    const wrapper = setup()
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1)
+  })
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup()
+    mockGetSecretWord.mockClear()
+
+    //using setProps because wrapper.update() doesn't trigger useEffect
+    // https://github.com/enzymejs/enzyme/issues/2254
+
+    wrapper.setProps()
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0)
+  })
+})
